2f848bec06c8bf91b7118e531e2696f0bf95b857
Boluwatife Dada <boluwatife.dada@stu.ocsb.ca>
2025-03-05 01:23:02 +0000

    pizza_cost.py

---
+++ constants.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# Created By: Boluwatife Dada
+# Date: feb 26, 2025
+# This program contains constants.
+HST = 0.13
+LABOUR_COST = 2.00
+RENTAL_COST = 2.25
+INGRED_COST = 1.15
